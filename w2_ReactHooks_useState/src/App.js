d3560560fbc6faba740b997d5cdad356a453fe6e
eunseo9311 <songeunseo@soongsil.ac.kr>
2024-11-14 06:05:57 +0900

    [chore] useState 구조잡기
    리액트 useState 구조잡기

--- w2_ReactHooks_useState/src/App.js
+++ w2_ReactHooks_useState/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tine, setTime] = useState(1);
+
+  const handClick = () => {
+    SVGFEDistantLightElement(time + 1};
+  };
+
+  console.log('업데이트!!');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <span>현재 시각: 1시{time}시</span>
+      <button onClick={handleClick}>Update</button>
+    <div>
+
   );
+
 }
 
 export default App;

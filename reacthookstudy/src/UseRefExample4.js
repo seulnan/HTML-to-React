b1bb771b34e5dc3c850879f0fa0af5bbec35a75c
eunseo9311 <songeunseo@soongsil.ac.kr>
2024-11-14 08:26:53 +0900

    [코드추가] UseRefExample4

---
+++ reacthookstudy/src/UseRefExample4.js
@@ -0,0 +1,29 @@
+import {useEffect, useRef} from 'react';
+
+function UseRefEx4(){
+
+    //useref 4
+    const inputRef = useRef();
+
+    useEffect(()=>{
+        console.log(inputRef);
+        inputRef.current.focus();
+    }, []);
+
+    const login=()=>{
+        alert(`환영합니다 ${inputRef.current.value}!`)
+        inputRef.current.focus();
+    }
+
+    return(
+
+    //useref 4
+    <div>
+        <input ref={inputRef} type='text' placeholder = "username"/>
+        <button onClick = {login}>로그인</button>
+    </div>
+
+    );
+}
+
+export default UseRefEx4;

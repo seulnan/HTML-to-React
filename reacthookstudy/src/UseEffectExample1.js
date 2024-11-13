45d43495fb2bfa197b183f5138864b6d03d082a1
Huniiiiiii <rtg8464@gmail.com>
2024-11-14 01:38:06 +0900

    reacthook commit

---
+++ reacthookstudy/src/UseEffectExample1.js
@@ -0,0 +1,44 @@
+import {useState, useEffect} from 'react';
+
+function useEffectExample1(){
+
+    //useEffect 1
+    const [count, setCount] = useState(1);
+    const [name, setName] = useState('');
+
+    const handleCountUpdate = () => {
+        setCount(count + 1);
+    };
+
+    const handleInputChange = (e)=>{
+        setName(e.target.value);
+    };
+
+    useEffect(()=> {
+        console.log('렌더링');
+    });
+
+    useEffect(()=> {
+        console.log('count 변화');
+    }, [count]);
+
+    useEffect(()=> {
+        console.log('name 변화');
+    }, [name]);
+
+    useEffect(()=> {
+        console.log('마운팅');
+    }, []);
+
+    return(
+    //useEffect 1
+    <div>
+        <button onClick = {handleCountUpdate}>Update</button>
+        <span>count: {count}</span>
+        <input type ="text" value={name} onChange={handleInputChange} />
+        <span>name: {name}</span>
+    </div>
+    );
+}
+
+export default useEffectExample1;
